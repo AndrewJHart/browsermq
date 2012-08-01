ce577d634867228af72e3df4e2ddb1917b2c0e27
Alex Dreyer <alex.dreyer@cmgdigital.com>
2012-08-01 12:00:11 -0400

    properly initialize the broker

--- bin/ioserver.js
+++ bin/ioserver.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-var broker = require('../lib/broker').subscribe;
+var broker = require('../lib/broker');
 var config = require('../lib/config').config;
 
 var io = require('socket.io').listen(config.port);
 
+broker.init()
 
 io.sockets.on('connection', function (socket) {
   socket.on('subscribe_queue', function(data) {
-    subscribe(socket, io.sockets, data);
+    broker.subscribe(socket, io.sockets, data);
   });
 });
 
